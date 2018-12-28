6b385a6dcf4eb062b7ea01c7411493b666a32d5d
Kevin <kevin.obrien@lovehoney.co.uk>
2018-12-28 13:35:18 +0000

    ajax requests with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,19 @@
+import axios from 'axios';
+
 const API_KEY = '51bbacc0962aaa4fe426dbc8ab845ccd';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// Assign string to a const so it only needs to be updated in one place.
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// An action creator returning an action with type FETCH_WEATHER
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  // Do a get request on the URL we have created.
+  // axios.get() returns a promise
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER
+  }
+}
