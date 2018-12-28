39ac2018fa6f3c6ca7db902c9e40b04c007fbc59
Kevin <kevin.obrien@lovehoney.co.uk>
2018-12-28 11:06:30 +0000

    controlled components and binding context

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    // Have to bind the 'this' context when passing a callback function that
+    // uses 'this', such as this.setState in the onInputChange method. Here we're
+    // binding this.setState to the this context of the SearchBar component
+    // (otherwise the program doesn't know where 'this' refers to when the
+    // onInputChange method is called.)
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value });
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}/>
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
